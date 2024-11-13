6a1a1715355ce398bfeaa43c864d4703bf0bee10
tgeorge676 <timothy.george@utexas.edu>
2024-11-13 16:04:37 -0600

    Shared memory reader

--- timothy.cpp
+++ timothy.cpp
@@ -4,9 +4,23 @@
 
 #include "header.h"
 
-namespace {}
+namespace {
+    pthread_t th;
+    void *func(void *shared_mem)
+    {
+        auto *data = static_cast<shared::SharedData*>(shared_mem);
+        std::cout << "Reading from memory:" << std::endl;
+        std::print("{}", data->as_string_view());
 
-void timothy::init(){}
+        return nullptr;
+    }
+}
 
-void timothy::run(){}
+void timothy::init(){
+    pthread_create(&th, nullptr, worker, shared::data);
+}
+
+void timothy::run(){
+    pthread_join(th, nullptr);
+}
 
