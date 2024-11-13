70d4c3a013131d1a33e1ce9decb29650ef01aa1f
Victoria Nguyen <victorianguyen@wireless-10-148-183-106.public.utexas.edu>
2024-11-13 16:08:09 -0600

    Added a message

--- victoria.cpp
+++ victoria.cpp
@@ -1,17 +1,28 @@
 //
-// Created by isaac on 11/13/24.
+// Created by victoria on 11/13/24.
 //
 
 #include "header.h"
 
 namespace
 {
+    pthread_t vt1;
+
+    void *myfunc(void *data) {
+        auto *s = static_cast<shared::SharedData*>(data);
+        char str[] = "Good evening everyone!";
+        memcpy(s, str, sizeof(str));
+        std::print("{}", s->as_string_view());
+        return nullptr;
+    }
 
 }
 
+
+
 void victoria::init()
 {
-
+    pthread_create(&vt1, nullptr, myfunc, shared::data);
 }
 
 void victoria::run()
